0835b1482ce7a3a760f9e3c4d95571c79b8a3e01
Mike Morales <mikemorale@live.com>
2018-02-09 23:41:33 -0700

    app nlstng

---
+++ app/data/friends.js
@@ -0,0 +1,50 @@
+var friends = [
+{
+	name: 'John J',
+	profilepic: 'your pick',
+	scores: ["1","2","3","4","5","5","4","3","4","5","3"]
+},
+{
+	name: 'Alex K',
+	profilepic: 'your =url pick ',
+	scores: ["2","5","1","3","4","3","4","4","5","4","3"]
+},
+{	name: 'Sally S'
+	profilepic: ["3","4","5","3","5","3","4","1","2","3"]
+}
+{
+	name: 'Doug',
+	profilepic: 'your pick',
+	scores: ["1","2","3","4","5","5","4","3","4","5","3"]
+},
+{
+	name: 'Jose',
+	profilepic: 'your =url pick ',
+	scores: ["2","5","1","3","2","3","5","1","5","2",]
+},
+{	name: 'Randall',
+	profilepic: [3]
+	scores: ["1","3","1","4","1","3","4","2","4","1","5"]
+},
+{
+	name: 'Earl',
+	profilepic: 'your pick',
+	scores: ["2","4","3","5","3","2","4","3","4","5","3"]
+},
+{
+	name: 'Dakota',
+	profilepic: 'your =url pick ',
+	scores: ["2","3","2","5","4","4","4","1","2","4","3"]
+},
+{	name: 'Rob'
+	profilepic: ["4","3","5","1","2","5","4","4","2","2"]
+}
+{
+	name: 'Sasha',
+	profilepic: 'your pick',
+	scores: ["5","5","3","4","3","2","4","5","2","4","4"]
+}];
+
+
+ // EXPORTS FRIENDS ARRAY 
+module.exports = friends;
